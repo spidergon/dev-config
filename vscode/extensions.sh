dbc9260cfe1e400c3787e93cc07acb2562b3b15f
Chris <spidergon@gmail.com>
2020-10-14 18:24:03 -0300

    added vscode extensions list

---
+++ vscode/extensions.sh
@@ -0,0 +1,34 @@
+# To generate the list:
+# code --list-extensions | sed -e 's/^/code --install-extension /' > vscode_extensions.sh
+
+code --install-extension aaron-bond.better-comments
+code --install-extension blanu.vscode-styled-jsx
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension BriteSnow.vscode-toggle-quotes
+code --install-extension bungcip.better-toml
+code --install-extension CoenraadS.bracket-pair-colorizer
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
+code --install-extension EditorConfig.EditorConfig
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension GraphQL.vscode-graphql
+code --install-extension howardzuo.vscode-npm-dependency
+code --install-extension jpoissonnier.vscode-styled-components
+code --install-extension lextudio.restructuredtext
+code --install-extension mikestead.dotenv
+code --install-extension Orta.vscode-jest
+code --install-extension Perkovec.emoji
+code --install-extension PKief.material-icon-theme
+code --install-extension ritwickdey.LiveServer
+code --install-extension ronnidc.nunjucks
+code --install-extension sdras.night-owl
+code --install-extension silvenon.mdx
+code --install-extension svelte.svelte-vscode
+code --install-extension toba.vsfire
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension wix.vscode-import-cost
+code --install-extension xabikos.ReactSnippets
+code --install-extension yzhang.markdown-all-in-one
